b0c79882a38f6247f204f009f286673d8d5e3e67
Jakub Piskiewicz <jakub@piskiewicz.org>
2021-01-25 10:48:05 +0100

    Projects gallery

--- src/Projects.js
+++ src/Projects.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './Projects.scss';
 
+class ProjectsGalleryItem extends React.Component {
+	render() {
+		let width = `width: ${this.props.progress}%`;
+
+		return (
+			<div className="projects-gallery-item">
+				<h3 className="project-name">{this.props.name}</h3>
+				<p className="project-description">{this.props.children}</p>
+				<div className="project-progress">
+					<span style={width}></span>
+				</div>
+			</div>
+		);
+	}
+}
+
+/* Vertically scrollable list of
+ * my most important projects.
+ * Every project is displayed as
+ * a interactable box. */
+class VerticalProjectsGrid extends React.Component {
+	render() {
+		return <div className="projects-vertical-grid"></div>;
+	}
+}
+
 class Projects extends React.Component {
-    render() {
-        return (
-            <div className="projects">
-                <div className="projects-section-name-container">
-                    <div>
-                        <p className="section-name">Projects</p>
-                        <p className="section-name">
-                            Progress on projects that I’m currently working on.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="projects">
+				<div className="projects-section-name-container">
+					<div>
+						<p className="section-name">Projects</p>
+						<p className="section-name">
+							Progress on projects that I’m currently working on.
+						</p>
+					</div>
+				</div>
+				<VerticalProjectsGrid />
+			</div>
+		);
+	}
 }
 
 export default Projects;
